ed0d0827a805a0a80a3e2798952be80b2cc17679
yuto <yutyn.tk24@gmail.com>
2021-11-19 16:37:10 +0900

    add:09

--- 08/calculator.php
+++ 08/calculator.php
@@ -21,12 +21,13 @@ if (!empty($_POST)) {
 <body>
     <h1>計算</h1>
     <?php if ($_SERVER["REQUEST_METHOD"] === "POST"): ?>
+        <h2><?=$num1?> + <?=$num2?> = <?=$result?></h2>
         
     <?php endif; ?>
-    <h2><?=$num1?> + <?=$num2?> = <?=$result?></h2>
     <form action="calculator.php" method="post" novalidate>
         <input type="text" name="num1" size="2" maxlength="2" value="<?=$num1?>"> +
         <input type="text" name="num2" size="2" maxlength="2" value="<?=$num2?>"> =
+        <input type="text" name="reslut" size="2" maxlength="2" value="<?=?>">
 
         <p><input type="submit" value="計算"></p>
     </form>
