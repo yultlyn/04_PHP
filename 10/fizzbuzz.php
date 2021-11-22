041826d412800164d6319419c9e613887a004638
yuto <yutyn.tk24@gmail.com>
2021-11-22 11:11:58 +0900

    updarte:10

--- 10/fizzbuzz.php
+++ 10/fizzbuzz.php
@@ -1,12 +1,13 @@
 <?php
- for($i = 1; $i <= 100; $i++){
-     if($i % 5 == 0 && $i % 3 == 0){
-         echo $i .'FizzBuzz' . '<br>';
-     }else if($i % 5 == 0){
-         echo $i .':Buzz'. '<br>';
-     }else if($i % 3 == 0){
-         echo $i . ':Fizz'. '<br>';
-     }else
-     echo $i . '<br>';
+$x = '<br>';
+
+ for ($i = 1; $i <= 100; $i++) {
+     if ($i % 5 == 0 && $i % 3 == 0) {
+         echo $i .':FizzBuzz' . $x;
+     } elseif ($i % 5 == 0){
+         echo $i .':Buzz'. $x;
+     } elseif ($i % 3 == 0){
+         echo $i . ':Fizz'. $x;
+     } else
+     echo $i . $x;
  }
-?>
